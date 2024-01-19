6839dd010f2d4fc52fb2510618a173f3bcce3fb0
yrichika <68413973+yrichika@users.noreply.github.com>
2024-01-19 14:23:34 +0900

    Add support for accepting piped input and command line arguments

--- cbcopy.go
+++ cbcopy.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"strings"
 
@@ -9,21 +10,33 @@ import (
 )
 
 func main() {
+
+	// 1. accept piped input
+	data, ioErr := io.ReadAll(os.Stdin)
+	if ioErr != nil {
+		fmt.Println("Error reading from stdin:", ioErr)
+		os.Exit(1)
+	}
+	if len(data) != 0 {
+		clipboard.Write(clipboard.FmtText, data)
+		fmt.Println("Copied to the clipboard!")
+		os.Exit(0)
+	}
+
+	// 2. accept arguments
 	args := os.Args[1:]
-
-	if (len(args)) == 0 {
-		fmt.Println("Nothing to copy!")
-		os.Exit(1)
+	if (len(args)) != 0 {
+		clipErr := clipboard.Init()
+		if clipErr != nil {
+			fmt.Println("Error initializing clipboard", clipErr)
+			os.Exit(1)
+		}
+		clipboard.Write(clipboard.FmtText, []byte(strings.Join(args, " ")))
+		fmt.Println("Copied to the clipboard!")
+		os.Exit(0)
 	}
 
-	fmt.Println("Copied to the clipboard!")
-	err := clipboard.Init()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	clipboard.Write(clipboard.FmtText, []byte(strings.Join(args, " ")))
-
-	os.Exit(0)
+	fmt.Println("Nothing to copy!")
+	os.Exit(1)
 
 }
