9a0ccabe22c3e02fdc4f1d147d6c4df2577d8008
yrichika <68413973+yrichika@users.noreply.github.com>
2024-01-20 23:32:19 +0900

    Make it works only for WSL

--- cbcopy.go
+++ cbcopy.go
@@ -4,40 +4,52 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"os/exec"
 	"strings"
-
-	"golang.design/x/clipboard"
 )
 
 func main() {
-	clipErr := clipboard.Init()
-	if clipErr != nil {
-		fmt.Println("Error initializing clipboard", clipErr)
-		os.Exit(1)
-	}
-
 	// 1. accept arguments
 	args := os.Args[1:]
-	if (len(args)) != 0 {
-		clipboard.Write(clipboard.FmtText, []byte(strings.Join(args, " ")))
+	if len(args) != 0 {
+		copyToClipboard(strings.Join(args, " "))
 		fmt.Println("Copied to the clipboard!")
 		os.Exit(0)
 	}
 
+	// 2. accept piped input
 	piped, ioErr := io.ReadAll(os.Stdin)
 	if ioErr != nil {
 		fmt.Println("Error reading from stdin:", ioErr)
 		os.Exit(1)
 	}
 
-	// 2. accept piped input
 	if len(piped) != 0 {
-		clipboard.Write(clipboard.FmtText, piped)
+		copyToClipboard(string(piped))
 		fmt.Println("Copied to the clipboard!")
 		os.Exit(0)
 	}
 
 	fmt.Println("Nothing to copy!")
 	os.Exit(1)
+}
 
+func copyToClipboard(text string) {
+	cmd := exec.Command("clip.exe")
+	in, err := cmd.StdinPipe()
+	if err != nil {
+		fmt.Println("Error creating stdin pipe:", err)
+		os.Exit(1)
+	}
+
+	go func() {
+		defer in.Close()
+		io.WriteString(in, text)
+	}()
+
+	err = cmd.Run()
+	if err != nil {
+		fmt.Println("Error running clip. Sorry, This command only works for WSL:", err)
+		os.Exit(1)
+	}
 }
