5cb06fdac9d6b5c6f1756746097e2052b51dd32f
yrichika <68413973+yrichika@users.noreply.github.com>
2024-01-20 23:29:11 +0900

    Reorder input handling logic in cbcopy.go

--- cbcopy.go
+++ cbcopy.go
@@ -10,29 +10,29 @@ import (
 )
 
 func main() {
-
 	clipErr := clipboard.Init()
 	if clipErr != nil {
 		fmt.Println("Error initializing clipboard", clipErr)
 		os.Exit(1)
 	}
+
+	// 1. accept arguments
+	args := os.Args[1:]
+	if (len(args)) != 0 {
+		clipboard.Write(clipboard.FmtText, []byte(strings.Join(args, " ")))
+		fmt.Println("Copied to the clipboard!")
+		os.Exit(0)
+	}
+
 	piped, ioErr := io.ReadAll(os.Stdin)
 	if ioErr != nil {
 		fmt.Println("Error reading from stdin:", ioErr)
 		os.Exit(1)
 	}
 
-	// 1. accept piped input
+	// 2. accept piped input
 	if len(piped) != 0 {
 		clipboard.Write(clipboard.FmtText, piped)
-		fmt.Println("Copied to the clipboard!")
-		os.Exit(0)
-	}
-
-	// 2. accept arguments
-	args := os.Args[1:]
-	if (len(args)) != 0 {
-		clipboard.Write(clipboard.FmtText, []byte(strings.Join(args, " ")))
 		fmt.Println("Copied to the clipboard!")
 		os.Exit(0)
 	}
